afcecb5e5362bee22b54c95d2e820a3df2663805
Sara Naeem <63316988+sara-naeem-cs@users.noreply.github.com>
2025-02-06 18:52:37 -0500

    Still need to add the page that shows resume

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+//import App from './App.jsx'
 import GeneralInformation from './GeneralInformation.jsx'
 import EducationSection from './EducationSection.jsx'
 import ExperienceSection from './ExperienceSection.jsx'
@@ -11,6 +11,5 @@ createRoot(document.getElementById('root')).render(
     <GeneralInformation></GeneralInformation>
     <EducationSection></EducationSection>
     <ExperienceSection></ExperienceSection>
-
   </StrictMode>
 )
